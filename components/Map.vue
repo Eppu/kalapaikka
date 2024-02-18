7a1090bd3d0fd361c2e54b43d93159c2dfd0fe23
Eetu Eskelinen <eetuesk@gmail.com>
2024-02-18 23:36:40 +0200

    Move zoom controls

--- components/Map.vue
+++ components/Map.vue
@@ -1,6 +1,7 @@
 <template>
   <div style="height: 100vh; width: 100vw">
-    <LMap ref="map" :zoom="zoom" :center="[64.89092, 25.92773]">
+    <LMap ref="map" :zoom="zoom" :center="[64.89092, 25.92773]" :options="mapOptions">
+      <LControlZoom position="bottomright" />
       <LTileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
@@ -13,6 +14,11 @@
 <script setup>
 import { ref } from 'vue';
 const zoom = ref(6);
+
+const mapOptions = {
+  zoomControl: false,
+  //   attributionControl: false,
+};
 </script>
 
 <style>
