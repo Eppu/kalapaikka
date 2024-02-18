0f213982d8d5e69cfe13d25f1eda10eb5df02f38
Eetu Eskelinen <eetuesk@gmail.com>
2024-02-18 23:19:50 +0200

    Add nuxt-mongoose

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt3-leaflet'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt3-leaflet',
+    'nuxt-mongoose',
+  ],
   tailwindcss: {
     configPath: 'tailwind.config.js',
     exposeConfig: false,
