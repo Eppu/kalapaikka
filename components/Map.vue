0cbcc07ef822b8056f7c2f3d6b7121a85c6ad98e
Eetu Eskelinen <eetuesk@gmail.com>
2024-02-19 00:41:37 +0200

    Add basic fetching to Map.vue

--- components/Map.vue
+++ components/Map.vue
@@ -7,18 +7,40 @@
         attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
         layer-type="base"
       />
+      <!-- Loop through markers and create a marker for each spot -->
+      <LMarker v-for="spot in fishingSpots" :key="spot._id" :lat-lng="spot.coordinates.coordinates">
+        <LPopup>
+          <div>
+            <h3>{{ spot.name }}</h3>
+            <p>{{ spot.createdAt }}</p>
+            <!-- Add other fields as needed -->
+          </div>
+        </LPopup>
+      </LMarker>
     </LMap>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-const zoom = ref(6);
+import { ref, onMounted } from 'vue';
 
+const zoom = ref(6);
 const mapOptions = {
   zoomControl: false,
-  //   attributionControl: false,
 };
+
+const fishingSpots = ref([]);
+
+onMounted(async () => {
+  // Fetch fishing spots from your API endpoint
+  try {
+    const response = await fetch('http://localhost:3000/api/v1/fishingspots');
+    const data = await response.json();
+    fishingSpots.value = data;
+  } catch (error) {
+    console.error('Error fetching fishing spots', error);
+  }
+});
 </script>
 
 <style>
