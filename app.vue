119c1f0f7f76bf33e6ea775743a488459b083ef4
Eetu Eskelinen <eetuesk@gmail.com>
2024-02-21 16:31:43 +0200

    Add basic Flowbite template

--- app.vue
+++ app.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <Map />
+
     <!-- <Counter /> -->
     <!-- <NuxtWelcome /> -->
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { initFlowbite } from 'flowbite';
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+});
+</script>
