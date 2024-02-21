c68b32c3dcd0317a0cfbbe90120011511557a812
Eetu Eskelinen <eetuesk@gmail.com>
2024-02-21 16:31:33 +0200

    Init Flowbite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'nuxt3-leaflet', 'nuxt-mongoose'],
   tailwindcss: {
-    configPath: 'tailwind.config.js',
-    exposeConfig: false,
-    exposeLevel: 2,
     config: {},
-    injectPosition: 'first',
     viewer: true,
+    configPath: 'tailwind.config.js',
   },
   mongoose: {
     uri: process.env.MONGO_URI,
     options: {},
     modelsDir: 'server/models',
     devtools: true,
-    debug: true,
   },
 });
