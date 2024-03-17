8d9a8a363de7d7c82965ee3ff01e4caa30e26a56
Eetu Eskelinen <eetuesk@gmail.com>
2024-03-17 21:04:10 +0200

    Use different layout for map page

---
+++ layouts/map.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex h-[calc(100dvh)] w-full flex-col">
+    <slot />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="flex h-[calc(100dvh)] w-full flex-col">
-    <!-- <div class="flex-grow"> -->
-    <Map />
-    <!-- </div> -->
-  </div>
+  <!-- <div class="flex-grow"> -->
+  <Map />
+  <!-- </div> -->
 </template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'map',
+});
+</script>
