9bf645bdc21fea1c6a182b49a72dfa75bb2c3182
Eetu Eskelinen <eetuesk@gmail.com>
2024-03-19 13:25:53 +0200

    Add nuxt-security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,19 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
     },
   },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt3-leaflet', 'nuxt-mongoose', '@formkit/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt3-leaflet', 'nuxt-mongoose', '@formkit/nuxt', 'nuxt-security'],
+  security: {
+    headers: {
+      // Needed for local development
+      crossOriginEmbedderPolicy: process.env.NODE_ENV === 'development' ? 'unsafe-none' : 'require-corp',
+      // Allow Leaflet map tiles
+      contentSecurityPolicy: {
+        'img-src': "'self' data: https://*.tile.openstreetmap.org",
+      },
+      xFrameOptions: 'DENY',
+    },
+  },
+
   tailwindcss: {
     config: {},
     viewer: true,
