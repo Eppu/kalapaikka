c02a52f624d2a2bbcd7da7c8647c6cee78bbd611
Eetu Eskelinen <eetuesk@gmail.com>
2024-03-21 17:18:23 +0200

    Refactor Navbar links to use a wrapper component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -31,21 +31,10 @@
           class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse"
         >
           <li>
-            <NuxtLink
-              to="/"
-              class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-              exact-active-class="  bg-blue-700 text-white md:bg-transparent md:text-blue-700 md:dark:text-blue-500 hover:bg-blue-600"
-              aria-current="page"
-              >Kartta</NuxtLink
-            >
+            <NavbarLink to="/" title="Kartta" />
           </li>
           <li>
-            <NuxtLink
-              to="/tietoa"
-              class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-              exact-active-class="  bg-blue-700 text-white md:bg-transparent md:text-blue-700 md:dark:text-blue-500 hover:bg-blue-600"
-              >Tietoa</NuxtLink
-            >
+            <NavbarLink to="/tietoa" title="Tietoa" />
           </li>
 
           <li>
---
+++ components/NavbarLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLink
+    :to="to"
+    class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+    exact-active-class="bg-blue-700 text-white md:bg-transparent md:text-blue-700 md:dark:text-blue-500 hover:bg-blue-600"
+    aria-current="page"
+  >
+    {{ title }}
+  </NuxtLink>
+</template>
+<script setup lang="ts">
+defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
