60c921517feba77af9f8aacd3269cacd6d64ec27
Eetu Eskelinen <eetuesk@gmail.com>
2024-03-28 13:13:34 +0200

    Update Tietoa page

--- pages/tietoa.vue
+++ pages/tietoa.vue
@@ -3,10 +3,14 @@
     <div class="flex max-w-screen-lg flex-col items-center space-y-6">
       <span class="self-center whitespace-nowrap text-sm font-semibold text-blue-700 dark:text-white">Tietoa</span>
       <h1 class="text-3xl font-semibold text-gray-800">Mikä ihmeen Kalakartta?</h1>
-      <p class="text-xl text-gray-700">
-        Kalakartta on palvelu, joka auttaa sinua löytämään parhaat pilkki- ja kalapaikat. Voit lisätä omia
-        kalapaikkojasi ja etsiä muiden lisäämiä paikkoja kartalta. Kalakartta on ilmainen ja avoin kaikille.
-      </p>
+      <div class="space-y-6 text-center text-xl text-gray-700">
+        <p>
+          Kalakartta on palvelu, joka auttaa sinua löytämään parhaat pilkki- ja kalapaikat lähelläsi. Voit merkitä
+          kartalle omia suosikkikalapaikkojasi, jakaa niitä muiden kalastajien kanssa ja lukea heidän kommenttejaan.
+        </p>
+
+        <p>Kalakartta on ilmainen ja avoin kaikille.</p>
+      </div>
     </div>
   </main>
 </template>
