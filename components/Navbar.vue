4772500e2bf3d83fcdd6b05c1cb616ba131f6999
Eetu Eskelinen <eetuesk@gmail.com>
2024-03-28 11:33:02 +0200

    Work on about page layout

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="border-gray-200 bg-white dark:bg-gray-900">
+  <nav class="border-gray-200 bg-white shadow-sm backdrop-saturate-200 dark:bg-gray-900">
     <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between px-4 py-2">
       <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <!-- TODO: Replace with SVG logo once we have a proper one -->
--- pages/tietoa.vue
+++ pages/tietoa.vue
@@ -1,1 +1,12 @@
-<template>Tietoa</template>
+<template>
+  <main class="container mx-auto flex h-full max-w-screen-xl justify-center px-4 py-16">
+    <div class="flex max-w-screen-lg flex-col items-center space-y-6">
+      <span class="self-center whitespace-nowrap text-sm font-semibold text-blue-700 dark:text-white">Tietoa</span>
+      <h1 class="text-3xl font-semibold text-gray-800">Mikä ihmeen Kalakartta?</h1>
+      <p class="text-xl text-gray-700">
+        Kalakartta on palvelu, joka auttaa sinua löytämään parhaat pilkki- ja kalapaikat. Voit lisätä omia
+        kalapaikkojasi ja etsiä muiden lisäämiä paikkoja kartalta. Kalakartta on ilmainen ja avoin kaikille.
+      </p>
+    </div>
+  </main>
+</template>
