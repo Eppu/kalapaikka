b4a7f2fa8c2eb2c5d77b3c178163cfee5a91eccd
Eetu Eskelinen <eetuesk@gmail.com>
2024-12-12 16:03:21 +0200

    Update about page

--- pages/tietoa.vue
+++ pages/tietoa.vue
@@ -3,16 +3,23 @@
     <div class="mx-auto flex max-w-screen-lg flex-col items-center space-y-6">
       <span class="self-center whitespace-nowrap text-sm font-semibold text-blue-700 dark:text-white">Tietoa</span>
       <h1 class="text-3xl font-semibold text-gray-800 max-sm:text-2xl">Mikä ihmeen Kalakartta?</h1>
-      <div class="space-y-6 text-center text-xl text-gray-700">
+      <div class="space-y-8 text-left text-xl text-gray-700 sm:text-center">
         <p>
           Kalakartta on palvelu, joka auttaa sinua löytämään parhaat pilkki- ja kalapaikat lähelläsi. Voit merkitä
           kartalle omia suosikkikalapaikkojasi, jakaa niitä muiden kalastajien kanssa ja lukea heidän kommenttejaan.
         </p>
-
         <p>Kalakartta on ilmainen ja avoin kaikille.</p>
+        <button
+          type="button"
+          class="mt-6 rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:bg-blue-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Avaa Kalakartta
+        </button>
       </div>
+      <div class="container mx-auto justify-center"></div>
     </div>
-    <div class="mt-40 flex flex-col space-y-6 max-md:mt-20">
+
+    <div class="mt-36 flex flex-col space-y-6 max-md:mt-20">
       <h2 class="text-2xl font-semibold text-gray-800">Miten Kalakartta toimii?</h2>
       <div class="space-y-6 text-lg text-gray-700">
         <p>
